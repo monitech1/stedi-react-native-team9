d13764f4be2652865bd5202b7f964053603f6275
Ernest Li <li23002@byui.edu>
2023-10-17 16:00:48 +0800

    W06 Automate Testing Multiple Name Functions

---
+++ utils/hello.ernest.js
@@ -0,0 +1,13 @@
+export const sayHello=() =>{
+    console.log('hello');
+    return 'hello';
+  }
+  
+export const sayName=() =>{
+    console.log('Ernest');
+    return 'Ernest';
+   }
+
+//For Manual Testing, Remove for Automated Testing
+
+export default sayHello;
